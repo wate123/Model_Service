bb83a1f150c477873a006112acfd9a972955e6f7
wate123 <junlin0604@gmail.com>
2018-04-06 19:05:42 -0500

    email sign up feature done

--- js/signup.js
+++ js/signup.js
@@ -1,19 +1,19 @@
-function emailSignup(){
-    var email = document.getElementById('username').value;
-    var password = document.getElementById('password').value;
-    firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
-        // Handle Errors.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        if(errorCode === 'auth/email-already-in-use'){
-            alert('This email is already in use.');
-        }else if(errorCode === 'auth/invalid-email') {
-            alert('The password is too weak.');
-        }else if(errorCode === 'auth/weak-password') {
-            alert('The password is too weak.');
-        }else{
-            alert(errorMessage)
-        }
-        console.log("sucess");
-    });
-}
+// function emailSignup(){
+//     var email = document.getElementById('email').value;
+//     var password = document.getElementById('password').value;
+//     firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+//         // Handle Errors.
+//         var errorCode = error.code;
+//         var errorMessage = error.message;
+//         if(errorCode === 'auth/email-already-in-use'){
+//             alert('This email is already in use.');
+//         }else if(errorCode === 'auth/invalid-email') {
+//             alert('The password is too weak.');
+//         }else if(errorCode === 'auth/weak-password') {
+//             alert('The password is too weak.');
+//         }else{
+//             alert(errorMessage)
+//         }
+//         console.log("sucess");
+//     });
+// }
